d0bd33604d9f7fc5c388031efa43193463a5570e
Heaq <janehesiyao@gmail.com>
2020-12-06 14:20:18 +0800

    add prepared stmt

---
+++ src/com/company/controller/commands/Retrieve.java
@@ -0,0 +1,29 @@
+package com.company.controller.commands;
+import java.io.*;
+import java.sql.*;
+import oracle.jdbc.driver.*;
+import oracle.sql.*;
+
+import com.company.Application;
+public class Retrieve {
+    public static Statement stmt;
+    public PreparedStatement preStmt;
+    public static OracleConnection conn = Application.conn;
+
+    static {
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static ResultSet retrieve(String sql) throws SQLException {
+        ResultSet rset = stmt.executeQuery(sql);
+        return rset;
+    }
+
+    public Retrieve(String prepared){
+
+    }
+}
